d3a71ce21a40072c517e6028952956539a232203
TiernanJesrani <134875734+TiernanJesrani@users.noreply.github.com>
2024-03-24 13:56:44 -0400

    added get_nearby for attractions, restaurants and hotels

--- tripadvisor.py
+++ tripadvisor.py
@@ -15,15 +15,49 @@ llm = ChatOpenAI(
 TRIPADVISOR_API_KEY = os.environ.get("TRIPADVISOR_API_KEY")
 
 @tool
-def get_nearby(latLong: str) -> str:
-    """A tool that allows you to get attractions and restaurants close to a given lattitude and longitude pair. The input should just be a pair of floats that looks like this: 51.5074, -0.1278. Do not add any other text."""
+def get_nearby_attraction(latLong: str) -> str:
+    """A tool that allows you to get attractions close to a given lattitude and longitude pair. The input should just be a pair of floats that looks like this: 51.5074, -0.1278. Do not add any other text."""
 
     url = f'https://api.content.tripadvisor.com/api/v1/location/nearby_search'
 
     headers = {"accept": "application/json"}
 
     print(f'LatLong provided: {latLong}')
-    response = requests.get(url, params={"latLong": latLong, "key": TRIPADVISOR_API_KEY}, headers=headers).json()
+    response = requests.get(url, params={"latLong": latLong, "key": TRIPADVISOR_API_KEY, "category":"attractions"}, headers=headers).json()
+ 
+    return_string = ""
+    print(response)
+    for location in response['data']:
+        return_string += f"Location ID: {location['location_id']}, Name: {location['name']}\n"
+    return return_string
+
+@tool
+def get_nearby_hotel(latLong: str) -> str:
+    """A tool that allows you to get hotels close to a given lattitude and longitude pair. The input should just be a pair of floats that looks like this: 51.5074, -0.1278. Do not add any other text."""
+
+    url = f'https://api.content.tripadvisor.com/api/v1/location/nearby_search'
+
+    headers = {"accept": "application/json"}
+
+    print(f'LatLong provided: {latLong}')
+    response = requests.get(url, params={"latLong": latLong, "key": TRIPADVISOR_API_KEY, "category":"hotels"}, headers=headers).json()
+ 
+    return_string = ""
+    print(response)
+    for location in response['data']:
+        return_string += f"Location ID: {location['location_id']}, Name: {location['name']}\n"
+    return return_string
+
+@tool
+def get_nearby_restaurants(latLong: str) -> str:
+    """A tool that allows you to get restaurants close to a given lattitude and longitude pair. The input should just be a pair of floats that looks like this: 51.5074, -0.1278. Do not add any other text."""
+
+    url = f'https://api.content.tripadvisor.com/api/v1/location/nearby_search'
+
+    headers = {"accept": "application/json"}
+
+    print(f'LatLong provided: {latLong}')
+    response = requests.get(url, params={"latLong": latLong, "key": TRIPADVISOR_API_KEY, "category":"restaurants"}, headers=headers).json()
  
     return_string = ""
     print(response)
@@ -52,7 +86,7 @@ Features: {response.get("features", None)}
 Amenities: {response.get("amenities", None)}
 """
 
-tools = [get_nearby, get_location_info]
+tools = [get_nearby_attraction, get_location_info, get_nearby_hotel, get_nearby_restaurants]
 
 from langchain.agents import initialize_agent
 
@@ -61,9 +95,9 @@ zero_shot_agent = initialize_agent(
     tools=tools,
     llm=llm,
     verbose=True,
-    max_iterations=3
+    max_iterations=30
 )
 
 if __name__ == "__main__":
     # print(get_location_info("2361377"))
-    zero_shot_agent("What are some locations near Paris and give me more information about them?")
+    zero_shot_agent("What are some hotels, restaurants, and attractions near Paris and give me more information about them?")
