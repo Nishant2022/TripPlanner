adb25935def78f4ddd4ebd805d00fa79316b3105
Nishant Dash <nishant.dash@gmail.com>
2024-03-24 13:20:38 -0400

    Added get_location_info tool

--- tripadvisor.py
+++ tripadvisor.py
@@ -12,22 +12,46 @@ llm = ChatOpenAI(
     model_name="gpt-3.5-turbo"
 )
 
+TRIPADVISOR_API_KEY = os.environ.get("TRIPADVISOR_API_KEY")
+
 @tool
 def get_nearby(latLong: str) -> str:
-    """A tool that allows you to get attractions and restaurants close to a given lattitude and longitude pair. The input should just be a pair of floats without parentheses or extra info."""
-    
-    url = f'https://api.content.tripadvisor.com/api/v1/location/nearby_search?key={os.environ.get("TRIPADVISOR_API_KEY")}'
+    """A tool that allows you to get attractions and restaurants close to a given lattitude and longitude pair. The input should just be a pair of floats without parentheses."""
+
+    url = f'https://api.content.tripadvisor.com/api/v1/location/nearby_search'
 
     headers = {"accept": "application/json"}
 
-    response = requests.get(url, params={"latLong": latLong}, headers=headers).json()
-    
+    print(f'LatLong provided: {latLong}')
+    response = requests.get(url, params={"latLong": latLong, "key": TRIPADVISOR_API_KEY}, headers=headers).json()
+ 
     return_string = ""
+    print(response)
     for location in response['data']:
         return_string += f"Location ID: {location['location_id']}, Name: {location['name']}\n"
     return return_string
-    
-tools = [get_nearby]
+
+@tool
+def get_location_info(location_id: str) -> str:
+    """A tool that returns information about a location. The input should be a location id string"""
+    url = f'https://api.content.tripadvisor.com/api/v1/location/{location_id}/details'
+
+    headers = {"accept": "application/json"}
+
+    response = requests.get(url, params={"key": TRIPADVISOR_API_KEY}, headers=headers).json()
+    print(response)
+    return f"""
+Name: {response["name"]}
+Description: {response["description"]}
+Phone: {response["phone"]}
+Website: {response["website"]}
+Rating: {response["rating"]}
+Price Level: {response["price_level"]}
+Hours:
+Features: {response["features"]}
+"""
+
+tools = [get_nearby, get_location_info]
 
 from langchain.agents import initialize_agent
 
@@ -40,4 +64,5 @@ zero_shot_agent = initialize_agent(
 )
 
 if __name__ == "__main__":
-    zero_shot_agent("What are some locations near London?")
+    # print(get_location_info("2361377"))
+    zero_shot_agent("What are some locations near London and give me more information about them?")
